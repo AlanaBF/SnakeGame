44e706cf4e37e709aaaec162a5be161f2949f079
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2024-08-29 18:51:14 +0100

    addition of game over

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
-
 game_is_on = True
 
 while game_is_on:
@@ -33,16 +31,20 @@ while game_is_on:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
+        
     # Detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        scoreboard.game_over()    
+        scoreboard.game_over()
+        scoreboard.reset() 
+        snake.reset()  
     
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
             scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+            
         
         
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,12 +1,15 @@
 from turtle import Turtle
+import time
 ALIGNMENT = "left"
 FONT = ("Courier", 24, "normal")
-
+HIGH_SCORE = 0
 class Scoreboard(Turtle):
     
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", mode="r") as data:
+            self.high_score = int(data.read())
         self.color("hotpink") 
         self.penup()
         self.goto(-280, 260)
@@ -15,13 +18,24 @@ class Scoreboard(Turtle):
     
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.goto(-280, 260)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+        with open("data.txt", mode="w") as data:
+            data.write(str(self.high_score))
+            
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
         
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.update_scoreboard()
-    
+        self.update_scoreboard()      
+         
     def game_over(self):
         self.goto(0, 0)
         self.write("GAME OVER", align="center", font=FONT)
+        self.getscreen().update()
+        time.sleep(2)  # Optional: Pause for 2 seconds to let the player see the message
+        self.clear()
